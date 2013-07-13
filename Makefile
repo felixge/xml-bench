db5b3e05eda9f23baa04cb5fa3bf83275dd21b14
Felix Geisendörfer <felix@debuggable.com>
2013-07-13 21:47:08 +0200

    Initial libxml2 benchmark
    Seems to be very slow right now, ~700 ops / sec. Will try to reuse the
    xml reader next.

--- Makefile
+++ Makefile
@@ -1,4 +1,16 @@
-all:
+export PKG_CONFIG_PATH := /usr/local/opt/libxml2/lib/pkgconfig/
+valgrind=/usr/local/Cellar/valgrind/3.8.1/bin/valgrind --suppressions=osx.supp --leak-check=full
+gcc=gcc -Wall -g
+
+all: bench_go bench_libxml2
+
+bench_go:
 	go test -test.bench . | tee results.go.txt
 
-.PHONY: all
+libxml2.txt: libxml2
+	$(valgrind) ./$^ | tee $@
+
+libxml2: libxml2.c
+	$(gcc) $^ `pkg-config --cflags libxml-2.0` `pkg-config --libs libxml-2.0` -o $@
+
+.PHONY: all libxml2.txt
---
+++ libxml2.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <time.h>
+#include <libxml/xmlreader.h>
+
+char *read_file_contents(char *name);
+int readStreamHeader(char *xml);
+const int loop_size = 100;
+const int samples = 100;
+
+int main() {
+  printf("run\tops_per_sec\tloop_size\n");
+  char *example_xml = read_file_contents("example.xml");
+  xmlInitParser();
+
+  int num = 0;
+  while (num < samples) {
+    num++;
+
+    int i;
+    clock_t start = clock();
+    for (i = 0; i < loop_size; i++) {
+      readStreamHeader(example_xml);
+    }
+
+    float sec = ((float)clock() - (float)start) / CLOCKS_PER_SEC;
+    float ops_per_sec = 1 / (sec / loop_size);
+    printf("%d\t%f\t%d\n", num, ops_per_sec, loop_size);
+    fflush(stdout);
+  }
+
+  free(example_xml);
+  xmlCleanupParser();
+  return 0;
+}
+
+char *read_file_contents(char *name) {
+  char *file_contents;
+  long input_file_size;
+  FILE *input_file = fopen(name, "rb");
+  fseek(input_file, 0, SEEK_END);
+  input_file_size = ftell(input_file);
+  rewind(input_file);
+  file_contents = malloc(input_file_size * (sizeof(char))+1);
+  fread(file_contents, sizeof(char), input_file_size, input_file);
+  fclose(input_file);
+  file_contents[input_file_size] = 0;
+  return file_contents;
+}
+
+int readStreamHeader(char *xml) {
+  xmlTextReaderPtr reader;
+  int ret;
+
+  reader = xmlReaderForMemory(xml, strlen(xml), "", NULL, 0);
+  if (reader == NULL) {
+    return 1;
+  }
+
+  ret = xmlTextReaderRead(reader);
+  if (ret == 1) {
+    xmlChar *name = xmlTextReaderName(reader);
+    assert(strcmp(name, "stream:stream") == 0);
+    free(name);
+  } else {
+    return 2;
+  }
+  xmlFreeTextReader(reader);
+  return 0;
+}
